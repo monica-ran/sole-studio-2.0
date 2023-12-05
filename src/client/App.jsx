b662b464601f17499ad0b26f120008fae95d8e67
carterp2112 <st0pandsm3lltheroses@gmail.com>
2023-12-05 09:53:12 -0600

    added packages

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -6,12 +6,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className='App'>
-        <h1>Boilerplate</h1>
-        <img id='comp-img' src='./computer.png'></img>
-        <p>Replace the starter code in this template with something cool</p>
-        <Login />
-    </div>
+    
   );
 }
 
