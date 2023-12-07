6195f2cf8e1efb154531a6ffbd1965837dc748a5
Nicole Park <nicolepark@bellsouth.net>
2023-12-07 18:19:12 -0500

    changes

--- src/server/db/users.js
+++ src/server/db/users.js
@@ -51,6 +51,30 @@ const getAllUsers = async () => {
     }
 };
 
+// async function getUserById(id) {
+//     // first get the user
+//     try {
+//         const {
+//             rows: [user],
+//         } = await db.query(
+//             `
+//         SELECT *
+//         FROM users
+//         WHERE id = $1;
+//       `,
+//             [id]
+//         );
+//         // if it doesn't exist, return null
+//         if (!user) return null;
+//         // if it does:
+//         // delete the 'password' key from the returned object
+//         // delete user.password;
+//         return user;
+//     } catch (error) {
+//         throw error;
+//     }
+// }
+
 async function getUserById(userId) {
     // first get the user
     try {
