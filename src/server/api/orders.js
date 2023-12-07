2139ffbd4857ce9f9538b3b5b61d1fc2c56e8ad9
Maverick Tomes <maverick.tomes@gmail.com>
2023-12-07 15:07:09 -0500

    copying more files to new branch

--- src/server/api/orders.js
+++ src/server/api/orders.js
@@ -1,52 +1,27 @@
 
-//app.js
-
 const express = require('express');
-const bodyParser = require('body-parser');
-const orders = require('./orders');
+const ordersRouter = express.Router()
 
-const app = express();
+const { requireUser } = require("./utils")
+const { createOrder, findActiveOrder, updateOrder, addProductToActiveOrder, removeProductFromActiveOrder} = require("../db")
+ 
 
-app.use(bodyParser.json());
+ordersRouter.get("/cart", requireUser, async (req, res, next) => {
+    try {
+       const cart = await findActiveOrder();
+       if (cart) {
+        res.send(cart)
+       } else {
+        createOrder()
+       }
 
-app.get('/orders', (req, res) => {
-    res.json(orders.getAllOrders());
-});
-
-app.get('/orders/:id', (req, res) => {
-    const order = orders.getSingleOrder(req.params.id);
-    if (!order) {
-        return res.status(404).json({ error: 'Order not found' });
+    }catch (err) {
+        next (err)
     }
+})
 
-    res.json(order);
-});
 
-app.post('/orders', (req, res) => {
-    const order = orders.postOrder(req.body);
-    res.status(201).json(order);
-});
 
-app.patch('/orders/:id', (req, res) => {
-    const order = orders.patchOrder(req.params.id, req.body);
-    if (!order) {
-        return res.status(404).json({ error: 'Order not found' });
-    }
 
-    res.json(order);
-});
 
-app.delete('/orders/:id', (req, res) => {
-    const order = orders.getOrder(req.params.id);
-    if (!order) {
-        return res.status(404).json({ error: 'Order not found' });
-    }
-
-    const index = orders.indexOf(order);
-    orders.splice(index, 1);
-
-    res.status(204).end();
-});
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+module.exports = ordersRouter;
