1687517ed3555d8d6863eb7fc3ece7ee3ae43d24
Fern Coelho <123507401+fernco@users.noreply.github.com>
2023-12-08 08:09:13 -0800

    test

--- src/client/components/index.js
+++ src/client/components/index.js
@@ -8,5 +8,4 @@ import Login from './Login';
 import Signup from './Signup';
 import Footer from "./Footer";
 
-export { Navigation, Home, Products, Account, About, Cart, Login, Signup };
-export { Navigation, Home, Products, Account, About, Cart, Footer };
+export { Navigation, Home, Products, Account, About, Cart, Login, Signup, Footer};
