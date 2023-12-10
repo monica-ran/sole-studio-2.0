1f360530636debe4fbb80862e6ef4d72c07f831b
Nicole Park <nicolepark@bellsouth.net>
2023-12-10 14:55:39 -0500

    code for account, shoe list, users list

--- src/client/components/Account.jsx
+++ src/client/components/Account.jsx
@@ -1,3 +1,49 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 export default function Account() {
-    return <h1>Account</h1>
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+    async function fetchProducts() {
+        let API = "http://localhost:3000/api";
+
+        try {
+            const response = await axios.get(`${API}/products`);
+            console.log(response);
+            setProducts(response.data);
+        } catch (err) {
+            console.error("Error fetching products:", err);
+        }
+    }
+
+    async function removeProduct(id) {
+        let API = "http://localhost:3000/api";
+
+        try {
+            await axios.delete(`${API}/products/${id}`);
+
+            fetchProducts();
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    return (
+        <ul>
+            {products.map((product) => (
+                <li key={product.id}>
+                    <h4>#{product.id}</h4>
+                    <h4>{product.name}</h4>
+                    <h4>{product.description}</h4>
+                    <h4>${product.price}</h4>
+                    <button onClick={() => removeProduct(product.id)}>X</button>
+                    <button>Edit</button>
+                </li>
+            ))}
+        </ul>
+    );
 }
