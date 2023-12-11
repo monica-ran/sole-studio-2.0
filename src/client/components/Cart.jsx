adf08f12df7c9ae23bcfd0ae2644565aa53c77f9
Fern Coelho <123507401+fernco@users.noreply.github.com>
2023-12-11 12:27:58 -0800

    tweaked the quanity on checkout page - going to work on total next

--- src/client/components/Cart.jsx
+++ src/client/components/Cart.jsx
@@ -13,14 +13,37 @@ function Cart() {
       const carts = JSON.parse(localStorage.getItem('cart')) || [];
       console.log('Cart items:', carts);
 
+  // Example: when adding an item to the cart
+  const addItemToCart = (item) => {
+  const updatedCart = [...carts, { ...item, quantity: 1 }]; // Ensure quantity is defined
+  localStorage.setItem('cart', JSON.stringify(updatedCart));
+  navigate('/cart');
+};
+
+// handle changes of quanity 
+const handleQuantityChange = (id, newQuantity) => {
+  const updatedCart = carts.map((item) => {
+    if (item.id === id) {
+      return {
+        ...item,
+        quantity: newQuantity,
+      };
+    }
+    return item;
+  });
+  localStorage.setItem('cart', JSON.stringify(updatedCart));
+  // No need to navigate here since the input change won't trigger a route change
+};
+
+
   
       // update the total whenver the cart items change 
       useEffect(() => {
-        const total = carts.reduce((acc, item) => {
+        const calculatedTotal = carts.reduce((acc, item) => {
           return acc + item.price * item.quantity;
-        }, 0)
-        setTotal(total)
-      }, [carts])
+        }, 0);
+        setTotal(calculatedTotal);
+      }, [carts]);
   
         // Handle incrementing the quantity of an item in the cart
       const handleInc = (id) => {
@@ -74,7 +97,7 @@ function Cart() {
         <div className="container mx-auto mt-10">
         <div className="w-3/4 shadow-md my-10 flex-wrap">
           <div className="bg-white px-10 py-1">
-            <div className="flex justify-between border-b pb-8">
+            <div className="flex justify-between border-b pb-8" style={{ marginTop: '35px' }}>
               <h1 className="font-semibold text-2xl">Shopping Cart</h1>
               <h2 className="font-semibold text-2xl">{carts.length} Items</h2>
             </div>
@@ -88,7 +111,7 @@ function Cart() {
               <div className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5" key={cart.id}>
                 <div className="flex w-2/5">
                   <div className="w-20">
-                    <img className="h-24" src={cart?.image} alt={cart?.title} />
+                    <img className="h-24" src={cart?.image_url} alt={cart?.title} />
                   </div>
                   <div className="flex flex-col justify-between ml-4 flex-grow">
                     <span className="font-bold text-sm">{cart?.title}</span>
@@ -99,9 +122,8 @@ function Cart() {
                 <div className="flex justify-center w-1/5">
                   <svg className="fill-current text-gray-600 w-3 cursor-pointer" viewBox="0 0 448 512" onClick={() => handleDec(cart?.id)}><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
                   </svg>
-  
-                  <input className="mx-2 border text-center w-8" type="text" value={cart?.quantity} />
-  
+  {/* Initialize to 1 or another default value */}
+  <input className="mx-2 border text-center w-8" type="text" value={cart?.quantity || 1} onChange={(e) => handleQuantityChange(cart?.id, e.target.value)}/>  
                   <svg className="fill-current text-gray-600 w-3 cursor-pointer" onClick={() => handleInc(cart?.id)} viewBox="0 0 448 512">
                     <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
                   </svg>
@@ -119,14 +141,14 @@ function Cart() {
           <div id="summary" className="w-2/4 px-8 py-10 container">
             <h1 className="font-semibold text-2xl border-b pb-8">Order Summary</h1>
             <div className="flex flex-wrap justify-between mt-10">
-              <span className="font-semibold text-sm uppercase">Items {carts?.length}</span>
-              <span className="font-semibold text-sm">$ {total?.toFixed(2)}</span>
+              {/* <span className="font-semibold text-sm uppercase">Items {carts?.length}</span> */}
+              {/* <span className="font-semibold text-sm">${total.toFixed(2)}</span> */}
             </div>
-            <div className="py-7 mt-2">
+            {/* <div className="py-7 mt-2">
               <label htmlFor="promo" className="font-semibold inline-block text-sm uppercase">Promo Code</label>
               <input type="text" id="promo" placeholder="Enter your code" className="p-2 text-sm w-full" />
             </div>
-            <button className="bg-red-500 hover:bg-red-600 px-3 py-1 text-sm text-white uppercase">Apply</button>
+            <button className="bg-red-500 hover:bg-red-600 px-3 py-1 text-sm text-white uppercase">Apply</button> */}
             <div className="border-t mt-8">
               <div className="flex font-semibold justify-between py-6 text-sm uppercase">
                 <span>Total cost</span>
