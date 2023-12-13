255877a43b92b42f05dec93b36809d133717e12a
Fern Coelho <123507401+fernco@users.noreply.github.com>
2023-12-13 11:50:30 -0800

    added credit card

--- src/client/components/Cart.jsx
+++ src/client/components/Cart.jsx
@@ -149,52 +149,162 @@ useEffect(() => {
                     <h1 className="font-semibold text-2xl border-b pb-8">Order Summary</h1>
                     <div className="flex flex-wrap justify-between mt-10"></div>
                     <div className="border-t mt-8">
-                        <div className="flex font-semibold justify-between py-6 text-sm uppercase">
-                            <span>Total cost </span>
-                            <span>${cart.total.toFixed(2)}</span>
-                        </div>
-                        <button
-                            onClick={handleCheckout}
-                            className="bg-indigo-500 font-semibold hover:bg-indigo-600 py-3 p-2 text-sm text-white uppercase w-full"
+
+                        <div className="border-t mt-8">
+    <div className="flex font-semibold justify-between py-6 text-sm uppercase">
+        <span>Total cost </span>
+        <span>${cart.total.toFixed(2)}</span>
+    </div>
+
+    {/* Form for shipping information */}
+    <div className="flex justify-between">
+    {/* Left Form Section */}
+    <div className="w-1/2 p-5">
+        <form className="flex flex-wrap gap-3">
+            <label className="relative w-full flex flex-col">
+                <span className="font-bold mb-3">Name</span>
+                <input
+                    className="rounded-md peer pl-12 pr-2 py-2 border-2 border-gray-200 placeholder-gray-300"
+                    type="text"
+                    name="name"
+                    placeholder="Your Name"
+                />
+            </label>
+
+            <label className="relative flex-1 flex flex-col">
+                <span className="font-bold mb-3">Street Address</span>
+                <input
+                    className="rounded-md peer pl-12 pr-2 py-2 border-2 border-gray-200 placeholder-gray-300"
+                    type="text"
+                    name="street_address"
+                    placeholder="Street Address"
+                />
+            </label>
+
+            <label className="relative flex-1 flex flex-col">
+                <span className="font-bold mb-3">City, State, Zip</span>
+                <input
+                    className="rounded-md peer pl-12 pr-2 py-2 border-2 border-gray-200 placeholder-gray-300"
+                    type="text"
+                    name="city_state_zip"
+                    placeholder="City, State, Zip"
+                />
+            </label>
+        </form>
+    </div>
+
+    {/* Right Form Section */}
+    <div className="w-1/2 p-5">
+        <form className="flex flex-wrap gap-3">
+            <label className="relative w-full flex flex-col">
+                <span className="font-bold mb-3">Card number</span>
+                <input
+                    className="rounded-md peer pl-12 pr-2 py-2 border-2 border-gray-200 placeholder-gray-300"
+                    type="text"
+                    name="card_number"
+                    placeholder="0000 0000 0000"
+                />
+                {/* Card icon (optional) */}
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="absolute bottom-0 left-0 -mb-0.5 transform translate-x-1/2 -translate-y-1/2 text-black peer-placeholder-shown:text-gray-300 h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+                    />
+                </svg>
+            </label>
+
+            <label className="relative flex-1 flex flex-col">
+                <span className="font-bold mb-3">Expire date</span>
+                <input
+                    className="rounded-md peer pl-12 pr-2 py-2 border-2 border-gray-200 placeholder-gray-300"
+                    type="text"
+                    name="expire_date"
+                    placeholder="MM/YY"
+                />
+                {/* Expiry date icon (optional) */}
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="absolute bottom-0 left-0 -mb-0.5 transform translate-x-1/2 -translate-y-1/2 text-black peer-placeholder-shown:text-gray-300 h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+                    />
+                </svg>
+            </label>
+
+            <label className="relative flex-1 flex flex-col">
+                <span className="font-bold flex items-center gap-3 mb-3">
+                    CVC/CVV
+                    <span className="relative group">
+                        <span className="hidden group-hover:flex justify-center items-center px-2 py-1 text-xs absolute -right-2 transform translate-x-full -translate-y-1/2 w-max top-1/2 bg-black text-white">
+                            Hey ceci est une infobulle!
+                        </span>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-4 w-4"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
                         >
-                            Checkout
-                        </button>
-{/* form for credit card:-)) */}
-                        <form className="flex flex-wrap gap-3 w-full p-5 ml-15">
-  <label className="relative w-full flex flex-col">
-    <span className="font-bold mb-3">Card number</span>
-    <input className="rounded-md peer pl-12 pr-2 py-2 border-2 border-gray-200 placeholder-gray-300" type="text" name="card_number" placeholder="0000 0000 0000" />
-    <svg xmlns="http://www.w3.org/2000/svg" className="absolute bottom-0 left-0 -mb-0.5 transform translate-x-1/2 -translate-y-1/2 text-black peer-placeholder-shown:text-gray-300 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" />
-    </svg>
-  </label>
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
+                            />
+                        </svg>
+                    </span>
+                </span>
+                <input
+                    className="rounded-md peer pl-12 pr-2 py-2 border-2 border-gray-200 placeholder-gray-300"
+                    type="text"
+                    name="card_cvc"
+                    placeholder="•••"
+                />
+                {/* CVC/CVV icon (optional) */}
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="absolute bottom-0 left-0 -mb-0.5 transform translate-x-1/2 -translate-y-1/2 text-black peer-placeholder-shown:text-gray-300 h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
+                    />
+                </svg>
+            </label>
+        </form>
+    </div>
+</div>
 
-  <label className="relative flex-1 flex flex-col">
-    <span className="font-bold mb-3">Expire date</span>
-    <input className="rounded-md peer pl-12 pr-2 py-2 border-2 border-gray-200 placeholder-gray-300" type="text" name="expire_date" placeholder="MM/YY" />
-    <svg xmlns="http://www.w3.org/2000/svg" className="absolute bottom-0 left-0 -mb-0.5 transform translate-x-1/2 -translate-y-1/2 text-black peer-placeholder-shown:text-gray-300 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-    </svg>
-  </label>
-
-  <label className="relative flex-1 flex flex-col">
-    <span className="font-bold flex items-center gap-3 mb-3">
-      CVC/CVV
-      <span className="relative group">
-        <span className="hidden group-hover:flex justify-center items-center px-2 py-1 text-xs absolute -right-2 transform translate-x-full -translate-y-1/2 w-max top-1/2 bg-black text-white"> Hey ceci est une infobulle !</span>
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-        </svg>
-      </span>
-    </span>
-    <input className="rounded-md peer pl-12 pr-2 py-2 border-2 border-gray-200 placeholder-gray-300" type="text" name="card_cvc" placeholder="&bull;&bull;&bull;" />
-    <svg xmlns="http://www.w3.org/2000/svg" className="absolute bottom-0 left-0 -mb-0.5 transform translate-x-1/2 -translate-y-1/2 text-black peer-placeholder-shown:text-gray-300 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
-    </svg>
-  </label>
-</form>
+    {/* Checkout button */}
+    <button
+        onClick={handleCheckout}
+        className="bg-indigo-500 font-semibold hover:bg-indigo-600 py-3 p-2 text-sm text-white uppercase w-full"
+    >
+        Checkout
+    </button>
+</div>
                     </div>
-                </div>
+                </div> 
             </div>
         </div>
     );
